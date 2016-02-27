2725d7f2b20c1d78486fcee237139deb6416d2a2
Kevin Piacentini <kevinpiac@gmail.com>
2016-02-27 22:27:33 +0100

    Fixing ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,29 +6,32 @@
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/26 11:48:57 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/02/27 17:56:12 by kpiacent         ###   ########.fr       */
+/*   Updated: 2016/02/27 22:25:55 by kevin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strtrim(char const *s)
 {
 	char			*str;
 	unsigned int	i;
-	unsigned int	j;
 	size_t			len;
 	size_t			less;
 
 	i = 0;
-	j = 0;
-	len = 0;
 	less = 0;
-	while (ft_istoskip(" \n\t", s[i]))
+	len = ft_strlen(s);
+	while (ft_istoskip(" \n\t", s[i]) && s[i])
 		i++;
+	if (!s[i])
+		return ("");
+	while (ft_istoskip(" \n\t", s[--len]))
+		less++;
+	len = ft_strlen(s) - i - less;
 	if (!(str = ft_strnew(len)))
 		return (NULL);
-	ft_strcpy(str, &s[i]);
-	i = 0;
+	ft_strncpy(str, &s[i], len);
 	return (str);
 }
