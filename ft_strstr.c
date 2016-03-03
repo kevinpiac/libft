3d805c0a6c8560991cdd93dac0ac248aaa085509
Kevin Piacentini <kevinpiac@gmail.com>
2016-03-03 17:26:56 +0100

    Fixing ft_strstr.c

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 13:44:42 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/02/28 12:20:03 by kpiacent         ###   ########.fr       */
+/*   Updated: 2016/03/03 17:26:33 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,24 @@
 
 char	*ft_strstr(const char *big, const char *little)
 {
-	unsigned int	i;
-	unsigned int	j;
+	const char *bigcp;
+	const char *littlecp;
 
-	i = 0;
-	j = 0;
-	if (!little || *little == '\0')
+	if (!ft_strlen(little) || !little)
 		return ((char *)big);
-	while (big[i])
+	while (*big)
 	{
-		while (big[i + j] == little[j])
+		bigcp = big;
+		littlecp = little;
+		while (*bigcp == *littlecp)
 		{
-			if (!little[j + 1])
-				return ((char *)(&big[i]));
-			j++;
+			bigcp++;
+			littlecp++;
+			if (!*littlecp)
+				return ((char *)big);
+
 		}
-		i++;
+		big++;
 	}
 	return (NULL);
 }
