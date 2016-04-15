23e439d4e7704aecedfa52afc2f0341ca8dfbd23
Kevin Piacentini <kevinpiac@gmail.com>
2016-04-15 12:03:38 +0200

    Add ft_vectshowall to print all items' content (ending with lf)

--- ft_vectshowall.c
+++ ft_vectshowall.c
@@ -1,31 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vectaddfront.c                                  :+:      :+:    :+:   */
+/*   ft_vectshowall.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/04/15 09:49:54 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/04/15 11:25:26 by kpiacent         ###   ########.fr       */
+/*   Created: 2016/04/15 12:01:48 by kpiacent          #+#    #+#             */
+/*   Updated: 2016/04/15 12:01:57 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_vectaddfront(t_vector *v, void *item)
+void	ft_vectshowall(t_vector *v)
 {
 	int		i;
 
-	ft_vectresize(v);
-	if (v->total > 0)
+	i = 0;
+	while (i < v->total)
 	{
-		i = v->total + 1;
-		while (i)
-		{
-			v->items[i] = v->items[i - 1];
-			i--;
-		}
+		ft_putendl(v->items[i]);
+		i++;
 	}
-	v->items[0] = item;
-	v->total++;
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/03 10:37:12 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/04/15 10:16:48 by kpiacent         ###   ########.fr       */
+/*   Updated: 2016/04/15 12:01:00 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ t_vector			*ft_vectnew(void);
 void				ft_vectadd(t_vector *v, void *item);
 void				ft_vectaddfront(t_vector *v, void *item);
 void				ft_vectresize(t_vector *v);
+void				ft_vectshowall(t_vector *v);
 
 /*
 ** LINKED LISTS
