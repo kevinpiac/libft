b354f05d933a5c8a8b828bca34e09bf3a3e66cd2
Kevin Piacentini <kevinpiac@gmail.com>
2016-04-25 18:43:07 +0200

    Save for backup, should continue opm_init

--- opm_setoption.c
+++ opm_setoption.c
@@ -1,37 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   opm_init.c                                         :+:      :+:    :+:   */
+/*   opm_setoption.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/04/23 13:55:06 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/04/25 18:42:51 by kpiacent         ###   ########.fr       */
+/*   Created: 2016/04/25 18:13:07 by kpiacent          #+#    #+#             */
+/*   Updated: 2016/04/25 18:41:13 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_opm_params		*opm_init(t_vector *arm, t_vector *config)
+void	opm_setoption(t_opm_option *option)
 {
-	t_opm_params	*opm;
-	t_opm_option	*option;
-	int				i;
+	t_opm_option	*exist;
 
-	i = 1;
-	opm = (t_opm_params *)ft_memalloc(sizeof(t_opm_params) * 1);
+	if ((exist = opm_option_find(option)))
 
-	while (i < arm->total)
-	{
-		option = ft_vectget(arm, i);
-		if (!opm_findoption(config, option))
-		{
-			opm->error = true;
-			opm->error_code = -1;
-		}
-		else
-			config_option->is_set = true;
-	}
-	opm->config = config;
-	return (opm);
 }
