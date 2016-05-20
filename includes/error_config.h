e4b698d381d8b30fea69345260d6f993ffeac41e
Kevin Piacentini <kevinpiac@gmail.com>
2016-05-20 14:28:04 +0200

    Add simple error message handler

---
+++ includes/error_config.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_config.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/03 10:37:12 by kpiacent          #+#    #+#             */
+/*   Updated: 2016/04/28 14:37:01 by kpiacent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_CONFIG_H
+# define ERROR_CONFIG_H
+# define ERROR_DEFAULT_MSG "An error has been detected."
+# define ERROR_PROGNAME "add_your_progname_here"
+# define ERROR_USAGE "usage: [your custom usage] [here...]"
+#endif
